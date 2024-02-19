0921a100c1fbb931727161e89c8ce7c0a88780d8
Huyen Nguyen <huyen@Huyens-MacBook-Air.local>
2024-02-19 15:14:33 +0700

    feature(add-huyen-as-member): revert changes while doing exam

--- c.py
+++ c.py
@@ -1,13 +1,18 @@
 def hello_c():
     pass
-    # pass
-    # pass
-    # pass
-    # print("hello")
-
+    pass
+    pass
+    pass
+    print("hello")
+    
+    
+    
+    
+    
+    
 
 def calculate():
-    bank = 1
-    c = 2
-    deposit = 3
-    apple = int(bank) + int(c) - deposit
+    b= 1
+    c =2
+    deposit= 3
+    a = int( b ) + int(c) - deposit
